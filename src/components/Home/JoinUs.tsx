a715a7857a9835aeeeba932b82ddbbe3fcb2d236
haike <haike0513@gmail.com>
2024-10-22 16:03:37 +0800

    style: update bg

--- src/components/Home/JoinUs.tsx
+++ src/components/Home/JoinUs.tsx
@@ -2,12 +2,10 @@ import { Button } from "../ui/button";
 
 export  function JoinUs() {
   return (
-    <div className=" my-10">
-
-      
+    <div className=" my-10 p-4 sm:p-10">
       <div className="grid grid-cols-1 sm:grid-cols-2 h-40">
         <div className=" col-span-1">
-          <div>Become a Operator earn reward</div>
+          <div className=" text-xl font-medium">Become a Operator earn reward</div>
         </div>
         <div className=" col-span-1">
           <div>AOS is a AI interface verification and sampling network for Hetu protocol On Eigenlayer</div>
