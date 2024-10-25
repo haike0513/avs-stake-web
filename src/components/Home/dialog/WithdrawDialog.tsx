636c42f6cb72b5493c85e7e790ce249aa53b7069
haike <haike0513@gmail.com>
2024-10-25 16:41:27 +0800

    update withdraw dialog

--- src/components/Home/dialog/WithdrawDialog.tsx
+++ src/components/Home/dialog/WithdrawDialog.tsx
@@ -8,11 +8,21 @@ import { useAccount } from "wagmi";
 import { StakedToken } from "../StakedInfo";
 import { AssetMap } from "@/config/token";
 import { Button } from "@/components/ui/button";
+import { useForm } from "react-hook-form"
+import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
+import { Checkbox } from "@/components/ui/checkbox";
 
 export const WithdrawDialog = React.forwardRef<
   React.ElementRef<typeof Dialog>,
   React.ComponentPropsWithoutRef<typeof Dialog>
 >(({  ...props }, ref) => {
+
+
+  const form = useForm({
+    defaultValues: {
+      items: [] as string[],
+    }
+  });
 
 
   const account = useAccount();
@@ -32,21 +42,52 @@ export const WithdrawDialog = React.forwardRef<
   >
     <DialogContent ref={ref}>
       <div>Withdraw</div>
-      <div className="flex flex-col gap-4">
-        <div className=" flex flex-col gap-2 my-8">
-            {tvlStrategies.map((tvl) => {
-              return <StakedToken 
-              key={tvl[0]} 
-              icon={AssetMap[tvl[0]]?.logoUrl}
-              name={tvl[0]} 
-              amount={`${tvl[1] || 0}`}
-              />
-            })}
+      <Form {...form}>
+        <FormField 
+         control={form.control}
+         name="items"
+        render={({field}) => {
+          return         <div className="flex flex-col gap-4">
+          <div className=" flex flex-col gap-2 my-8">
+              {tvlStrategies.map((item) => {
+                return <FormItem 
+                key={item[0]} 
+               >
+                <div className=" flex items-center">
+                  <FormControl>
+                  <Checkbox
+                      checked={field.value?.includes(item[0])}
+                      onCheckedChange={(checked) => {
+                        return checked
+                          ? field.onChange([...field.value, item[0]])
+                          : field.onChange(
+                              field.value?.filter(
+                                (value: any) => value !== item[0]
+                              )
+                            )
+                      }}
+                    />
+                  </FormControl>
+                  <div className=" flex-grow">
+                      <StakedToken 
+                      key={item[0]} 
+                      icon={AssetMap[item[0]]?.logoUrl}
+                      name={item[0]} 
+                      amount={`${item[1] || 0}`}
+                      />
+                  </div>
+                  </div>
+               </FormItem>
+              })}
+            </div>
+            <div className=" w-full">
+            <Button className=" w-full">Withdraw</Button>
           </div>
-          <div className=" w-full">
-          <Button className=" w-full">Withdraw</Button>
         </div>
-      </div>
+        }}
+        />
+
+      </Form>
     </DialogContent>
   </Dialog>
 )
